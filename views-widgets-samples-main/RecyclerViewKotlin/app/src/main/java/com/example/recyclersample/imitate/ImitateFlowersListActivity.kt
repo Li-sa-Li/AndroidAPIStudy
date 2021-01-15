42f70e23550e62a8a3e06efc316e996745b2083d
lisa.li <lisa.li@meetsocial.cn>
2021-01-15 13:05:11 +0800

    add test

---
+++ views-widgets-samples-main/RecyclerViewKotlin/app/src/main/java/com/example/recyclersample/imitate/ImitateFlowersListActivity.kt
@@ -0,0 +1,11 @@
+package com.example.recyclersample.imitate
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ImitateFlowersListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_imitate_flowers_list)
+    }
+}
---
+++ views-widgets-samples-main/RecyclerViewKotlin/app/src/main/java/com/example/recyclersample/imitate/flowerlist/ImitateHeaderAdapter.kt
@@ -0,0 +1,4 @@
+package com.example.recyclersample.imitate.flowerlist
+
+class ImitateHeaderAdapter {
+}
